3109fca276fc5a54d1741605722a11bc35cf62fc
Carried <90163426+tapir132@users.noreply.github.com>
2023-10-29 12:51:30 -0700

    Added DM logging and help command.

--- src/events/mineflayer/chat/whisper.ts
+++ src/events/mineflayer/chat/whisper.ts
@@ -1,14 +1,34 @@
 import { Event } from "../../../interfaces/Event";
 import fetchHypixelGuild from "../../../util/requests/fetchHypixelGuild";
 import fetchMojangProfile from "../../../util/requests/fetchMojangProfile";
+import fetchHypixelPlayerProfile from "../../../util/requests/fetchHypixelPlayerProfile";
 import isFetchError from "../../../util/requests/isFetchError";
+import { TextChannel } from "discord.js";
 
 export default {
 	name: "chat:whisper",
 	runOnce: false,
 	run: async (bot, playerName: string, message: string) => {
-		const errorMessage = `/w ${playerName} There was an error attempting your request! (Check spelling and/or try again later)`;
+		const errorMessage = `/w ${playerName} There was an error! Message me "help" for a list of commands. (If you are attempting to message staff, please start your message with "Staff")`;
 		const target = message.startsWith("weeklygexp") || message.startsWith("weeklygxp") ? playerName : (message.split(" ")[0] as string);
+		const lowerMessage = message.toLowerCase();
+
+		if (lowerMessage.startsWith("staff")){
+            ((await bot.discord.channels.fetch(process.env.DM_LOG_CHANNEL_ID)) as TextChannel).send(`Message from ${playerName}: ${message}`);
+            bot.executeCommand(`/w ${playerName} Your message has been forwarded to staff. We will take a look at it as soon as possible.`);
+            return;
+        }
+        if (lowerMessage.startsWith("help")) {
+            setTimeout(() => {
+                bot.executeCommand(`/w ${playerName} To view your weekly GEXP, message me "weeklygexp".`);
+            }, 500);
+            setTimeout(() => {
+                bot.executeCommand(`/w ${playerName} To message staff, message the bot anything, but make sure to start your message with "Staff" ex: Staff I need help with guild reqs.`);
+            }, 3000);
+            return;
+        }
+
+        
 
 		const mojangProfile = await fetchMojangProfile(target);
 		if (isFetchError(mojangProfile)) {

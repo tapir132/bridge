81e27915454d0f187a44e647980fb03ed82ae78f
Carried <90163426+tapir132@users.noreply.github.com>
2023-10-30 20:33:08 -0700

    Small tweaks to make auto rank more adaptable to other guilds

--- src/commands/syncRanks.ts
+++ src/commands/syncRanks.ts
@@ -20,10 +20,14 @@ export default {
   },
   run: async (bot, interaction: CommandInteraction, args) => {
     const doDemotions = args[0];
-    const memberRequirement: number = Number(process.env.MEMBER_RANK_REQUIREMENT!);
-    //memberRequirement currently causes a compile error as it is not being used yet, but it does not affect code functionality
-    const eliteRequirement: number = Number(process.env.ELITE_RANK_REQUIREMENT!);
-    const fairRequirement: number = Number(process.env.FAIR_RANK_REQUIREMENT!);
+    const lowestRankRequirement: number = Number(process.env.LOWEST_RANK_REQUIREMENT!);
+    //lowestRankRequirement currently causes a compile error as it is not being used yet, but it does not affect code functionality
+    const middleRankRequirement: number = Number(process.env.MIDDLE_RANK_REQUIREMENT!);
+    const highestRankRequirement: number = Number(process.env.HIGHEST_RANK_REQUIREMENT!);
+
+    const lowestRankName = process.env.LOWEST_RANK_NAME
+    const middleRankName = process.env.MIDDLE_RANK_NAME
+    const highestRankName = process.env.HIGHEST_RANK_NAME
     
 
 
@@ -47,45 +51,47 @@ export default {
           const namesData = await namesResponse.json();
           const names = namesData.name;
           const guildRank = member.rank;
+          console.log(`Checked ${names} : ${total}`);
 
 
           // Find the rank requirement for the current member's rank
-          if (guildRank === 'Member' && total >= eliteRequirement) {
-            if (guildRank === 'Member' && total >= fairRequirement) {
-              console.log(`${names} should be promoted to Fair!`);
-              await bot.executeTask(`/g setrank ${names} Fair`);
-              actionSummary.push(`${names} promoted to Fair`);
+          if (guildRank === lowestRankName && total >= middleRankRequirement) {
+            if (guildRank === lowestRankName && total >= highestRankRequirement) {
+              console.log(`${names} should be promoted to ${highestRankName}!`);
+              await bot.executeTask(`/g setrank ${names} ${highestRankName}`);
+              actionSummary.push(`${names} promoted to ${highestRankName}`);
             }else{
-            console.log(`${names} should be promoted to Elite!`);
-            await bot.executeTask(`/g setrank ${names} Elite`);
-            actionSummary.push(`${names} promoted to Elite`);
+            console.log(`${names} should be promoted to ${middleRankName}!`);
+            await bot.executeTask(`/g setrank ${names} ${middleRankName}`);
+            actionSummary.push(`${names} promoted to ${middleRankName}`);
             }
           }
-          if (guildRank === 'Elite' && total >= fairRequirement) {
-            console.log(`${names} should be promoted to Fair!`);
-            await bot.executeTask(`/g setrank ${names} Fair`);
-            actionSummary.push(`${names} promoted to Fair`);
+          if (guildRank === middleRankName && total >= highestRankRequirement) {
+            console.log(`${names} should be promoted to ${highestRankName}!`);
+            await bot.executeTask(`/g setrank ${names} ${highestRankName}`);
+            actionSummary.push(`${names} promoted to ${highestRankName}`);
           }
-          if (guildRank === 'Elite' && total <= eliteRequirement && doDemotions === true) {
-            console.log(`${names} should be demoted to Member!`);
-            await bot.executeTask(`/g setrank ${names} Member`);
-            console.log (`${doDemotions}`)
-            actionSummary.push(`${names} demoted to Member`);
+          if (guildRank === middleRankName && total <= middleRankRequirement && doDemotions === true) {
+            console.log(`${names} should be demoted to ${lowestRankName}!`);
+            await bot.executeTask(`/g setrank ${names} ${lowestRankName}`);
+            actionSummary.push(`${names} demoted to ${lowestRankName}`);
           }
-          if (guildRank === 'Fair' && total <= fairRequirement && doDemotions === true) {
-            if (guildRank === 'Fair' && total <= eliteRequirement && doDemotions === true) {
-              console.log(`${names} should be demoted to Member!`);
-              console.log (`${doDemotions}`)
-              actionSummary.push(`${names} demoted to Member`);
+          if (guildRank === highestRankName && total <= highestRankRequirement && doDemotions === true) {
+            if (guildRank === highestRankName && total <= middleRankRequirement && doDemotions === true) {
+              console.log(`${names} should be demoted to ${lowestRankName}!`);
+              actionSummary.push(`${names} demoted to ${lowestRankName}`);
             }else{
-            console.log(`${names} should be demoted to Elite!`);
-            console.log (`${doDemotions}`)
-            actionSummary.push(`${names} demoted to Elite`);
+            console.log(`${names} should be demoted to ${middleRankName}!`);
+            actionSummary.push(`${names} demoted to ${middleRankName}`);
             }
+          }
+          if(total < lowestRankRequirement){
+            console.log(`${names} is inactive!`);
           }
         } else {
           console.error(`Error fetching names for UUID: ${uuid}`);
         }
+        
       }
 
       // Your response message after processing all members

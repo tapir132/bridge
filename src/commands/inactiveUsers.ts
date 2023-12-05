7dbe5c96617d3ac697f9d57b3e6d58c689293699
Carried <90163426+tapir132@users.noreply.github.com>
2023-12-04 20:04:28 -0800

    Renamed autoKick to inactiveUsers to more accurately reflect the command usage

--- src/commands/inactiveUsers.ts
+++ src/commands/inactiveUsers.ts
@@ -3,37 +3,29 @@ import fetch from 'node-fetch';
 import { Command } from '../interfaces/Command';
 import isFetchError from '../util/requests/isFetchError';
 import fetchHypixelGuild from '../util/requests/fetchHypixelGuild';
-import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+import { ApplicationCommandOptionType, EmbedBuilder, ButtonBuilder, ButtonComponent, ButtonStyle, ActionRowBuilder } from "discord.js";
 import fetchHypixelPlayerProfile from '../util/requests/fetchHypixelPlayerProfile';
+import fetchHypixelPlayerProfileUUID from '../util/requests/fetchHypixelPlayerProfileUUID';
 import * as fs from 'fs';
 import * as path from 'path';
 
 
 export default {
   data: {
-    name: 'autokick',
-    description: 'Automatically kick inactive users!',
-    options: [
-      {
-        name: "auto",
-        description: "Would you like to automatically kick users?",
-        type: ApplicationCommandOptionType.Boolean,
-        required: true,
-      },
-    ],
+    name: 'inactiveusers',
+    description: 'Automatically find inactive users yipee!',
   },
-  run: async (bot, interaction: CommandInteraction, args) => {
+  run: async (bot, interaction: CommandInteraction) => {
     const dnkl = path.join(__dirname, '../../dnkl.txt');
-    const doAutoKick = args[0];
     const lowestRankName = process.env.LOWEST_RANK_NAME;
     const ownerUUID = process.env.GUILD_MASTER_UUID ?? ")";
     const playerGuild = await fetchHypixelGuild(`${ownerUUID}`);
     if (isFetchError(playerGuild)) {
       return;
     }
-    
     await interaction.deferReply({ ephemeral: false });
     const actionSummary = [];
+
     try {
       for (const member of playerGuild.members) {
         const total = Object.values(member.expHistory).reduce((previous, current) => previous + current);
@@ -47,8 +39,30 @@ export default {
           if (total == 0) {
             const playerProfile = await fetchHypixelPlayerProfile(`${names}`);
             if (playerProfile == null && guildRank == lowestRankName) {
-              actionSummary.push(`${names} has recently changed their name thus is unable to be checked on the API.`);
-            }
+              console.log(`Attempting to bypass namechange error for ${names}...`);
+              const playerProfile = await fetchHypixelPlayerProfileUUID(`${uuid}`);
+              if (!isFetchError(playerProfile)) {
+                if (playerProfile.lastLogout == undefined && guildRank == lowestRankName) {
+                  actionSummary.push(`${names} has their API off!`);
+                } else if (playerProfile.lastLogout! > playerProfile.lastLogin!) {
+                  fs.readFile(dnkl, 'utf-8', (err, data) => {
+                    if (err) throw err;
+                    if(data.includes(names)){
+                      console.log(`${names} excluded`)
+                    } else{
+                      const currentTime = new Date().getTime();
+                      const thirtyDaysAgo = currentTime - 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
+                      if (playerProfile.lastLogout! <= thirtyDaysAgo) {
+                        if (guildRank == lowestRankName) {
+                          const lastLogoutInDays = ((currentTime - playerProfile.lastLogout!) / (1000 * 60 * 60 * 24)).toFixed(1);
+                          actionSummary.push(`${names} should be kicked! Last logout was ${lastLogoutInDays} days ago.`);
+                        } 
+                      }
+                    }
+                  });
+                }
+              }
+          }else{
             if (!isFetchError(playerProfile)) {
               if (playerProfile.lastLogout == undefined && guildRank == lowestRankName) {
                 actionSummary.push(`${names} has their API off!`);
@@ -61,29 +75,28 @@ export default {
                     const currentTime = new Date().getTime();
                     const thirtyDaysAgo = currentTime - 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
                     if (playerProfile.lastLogout! <= thirtyDaysAgo) {
-                      if (doAutoKick == true && guildRank == lowestRankName) {
-                        //bot.executeCommand(`/g kick ${names}`);
-                        const lastLogoutInDays = ((currentTime - playerProfile.lastLogout!) / (1000 * 60 * 60 * 24)).toFixed(1);
-                        actionSummary.push(`${names} was kicked! Last logout was ${lastLogoutInDays} days ago.`);
-                      } else if (guildRank == lowestRankName) {
+                      if (guildRank == lowestRankName) {
                         const lastLogoutInDays = ((currentTime - playerProfile.lastLogout!) / (1000 * 60 * 60 * 24)).toFixed(1);
                         actionSummary.push(`${names} should be kicked! Last logout was ${lastLogoutInDays} days ago.`);
-                      }
+                      } 
                     }
-
                   }
                 });
               }
             }
           }
+          }
         }
       }
+
       const embed = new EmbedBuilder()
         .setTitle('Summary')
-        .setDescription(actionSummary.length > 0 ? actionSummary.join('\n') : 'No actions were performed. If you think this is an error, you may be getting rate limited.');
+        .setDescription(actionSummary.length > 0 ? actionSummary.join('\n') : 'No actions were performed. If you think this is an error, you may be getting rate limited.')
+    
 
-      // Send the embed
-      await interaction.editReply({ embeds: [embed.toJSON()] });
+      await interaction.editReply({ 
+        embeds: [embed.toJSON()],  
+      });
 
     } catch (error) {
       console.error('An error occurred:', error);
